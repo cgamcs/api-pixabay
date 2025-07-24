ea45645a9b03c8b44752b854f9cc3847937f4f58
César García <cesar.garciamcs@outlook.com>
2025-07-23 18:48:01 -0600

    moviendo a async await la consulta

--- js/app.js
+++ js/app.js
@@ -44,19 +44,21 @@ function mostrarAlerta(mensaje) {
     }
 }
 
-function buscarImagenes() {
+async function buscarImagenes() {
     const termino = document.querySelector('#termino').value
 
     const key = '51107069-5acf2a92e7bc8ed91b2ba2566'
     const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registrosPorPagina}&page=${paginaActual}`
 
-    fetch(url)
-        .then(respuesta => respuesta.json())
-        .then(resultado => {
-            totalPaginas = calcularPaginas(resultado.totalHits)
-            console.log(totalPaginas)
-            mostrarImagenes(resultado.hits)
-        })
+    try {
+        const respuesta = await fetch(url)
+        const resultado = await respuesta.json()
+
+        totalPaginas = calcularPaginas(resultado.totalHits)
+        mostrarImagenes(resultado.hits)
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 // Generador que va a registrar la cantidad de elementos de acuredo a las paginas
